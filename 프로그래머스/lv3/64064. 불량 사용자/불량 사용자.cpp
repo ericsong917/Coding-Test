348d4b1d83ae47fe32509dc731839cd166a8cd7b
JungHyun Song <57216145+ericsong917@users.noreply.github.com>
2023-10-03 22:56:06 +0900

    [level 3] Title: 불량 사용자, Time: 0.03 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 #include<iostream>
 #include<algorithm>
+#include <set>
 using namespace std;
 int len;
 int ans=0;
-vector<vector<string>> vec;
+set<vector<string> > vec;
 int visited[10]={0,};
 vector<string>usr_id,bn_id;
 bool compare(string a, string b){ //a가 '*' 포함
@@ -22,36 +23,7 @@ bool compare(string a, string b){ //a가 '*' 포함
 void backtrack(int depth,vector<string> tmp){
     sort(tmp.begin(),tmp.end());
     if(depth == bn_id.size()){
-        if(vec.size()==0){
-            ans++;
-            vec.push_back(tmp);
-            for(auto e : tmp){
-                cout<<e<<' ';
-            }
-            cout<<'\n';
-            return;
-        }
-        else{
-            for(auto v2 : vec){ //현재 목록들
-                if(v2.size() != tmp.size())
-                    continue;
-                sort(v2.begin(),v2.end());
-                int flag=0;
-                for(int l=0;l<v2.size();l++){
-                    if(v2[l]!=tmp[l])
-                        flag=1;
-                }
-                //v2와 tmp가 일치하면 tmp는 추가될수 없다.
-                if(flag==0)
-                    return;
-            }
-        }
-        for(auto e : tmp){
-            cout<<e<<' ';
-        }
-        cout<<'\n';
-        ans++;
-        vec.push_back(tmp);
+        vec.insert(tmp);
         return;
     }
     for(int i=0;i<usr_id.size();i++){
@@ -72,5 +44,5 @@ int solution(vector<string> user_id, vector<string> banned_id) {
     usr_id = user_id;
     bn_id = banned_id;
     backtrack(0,tmp2);
-    return ans;
+    return vec.size();
 }
